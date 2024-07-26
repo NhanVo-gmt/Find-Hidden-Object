9eb948b2ae83f864f039ff8855a74a07e2b435a6
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-07-26 15:38:49 +0700

    adding dependency

---
+++ Assets/Scripts/UI/GameUI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUI : MonoBehaviour
+{
+    public void OpenShop()
+    {
+        
+    }
+
+    public void OpenSetting()
+    {
+        
+    }
+
+    public void OpenChest()
+    {
+        
+    }
+
+    public void GoToHomeScreen()
+    {
+        
+    }
+}
