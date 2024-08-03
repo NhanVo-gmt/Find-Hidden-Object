d739ece1a1763f062258df8a52b00352c5b0165d
NhanVoo <141220487+NhanVoo@users.noreply.github.com>
2024-08-03 22:29:21 +1000

    fixing bug

---
+++ Assets/Scripts/DIContexts/MainSceneInstaller.cs
@@ -0,0 +1,14 @@
+namespace DIContexts
+{
+    using GameFoundation.Scripts.UIModule.ScreenFlow.Managers;
+    using GameFoundation.Scripts.UIModule.Utilities;
+    using UserData.Controller;
+
+    public class MainSceneInstaller : BaseSceneInstaller
+    {
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+        }
+    }
+}
